96f94b4c019060cb5da5e56162d4a8d4f872f0b7
Lina <l.goerzen@outlook.com>
2026-03-09 22:03:51 +0100

    add endpoint for testing the deployed version

--- src/main/java/de/ftracker/controller/DemoController.java
+++ src/main/java/de/ftracker/controller/DemoController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,5 +24,10 @@ public class DemoController {
     @PostMapping("/start")
     public AuthResponse startDemo() {
         return demoService.startDemo();
+    }
+
+    @GetMapping("/deploy-check")
+    public String deployCheck() {
+        return "deploy-check-2026-03-09-22-15";
     }
 }
